fa65ac9bdd02d18cf2f02ab08d4d9c562ea665ad
KittitatNou-Ward <Kittiat2548@gmail.com>
2025-01-08 20:39:56 +0700

    update อีกรอบ

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,13 @@
-
-
 <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
 Route::get('/mycontroller', 
+[MyController::class, 'myfunction']
+);
+
+Route::post('/mycontroller', 
 [MyController::class, 'myfunction']
 );
 
