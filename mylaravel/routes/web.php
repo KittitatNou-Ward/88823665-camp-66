7491b677b39266bfcc93d1afe3e6b88789f21ad0
KittitatNou-Ward <Kittiat2548@gmail.com>
2025-01-08 18:01:51 +0700

    update
    new laravel

---
+++ mylaravel/routes/web.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('/mycontroller', 
+[MyController::class, 'myfunction']
+);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello/{id?}', function ($val="") {
+    return "<h1> Hello World! $val</h1>";
+});
