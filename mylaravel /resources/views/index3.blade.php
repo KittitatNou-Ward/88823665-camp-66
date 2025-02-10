8d85a4226adb34524ea206d1cd7c43a82185c8d2
KittitatNou-Ward <Kittiat2548@gmail.com>
2025-02-10 08:56:30 +0700

    d

---
+++ mylaravel /resources/views/index3.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index 3 Page</title>
+</head>
+<body>
+    <h1>Welcome to Index 3</h1>
+    <p>This is a sample page in Laravel.</p>
+</body>
+</html>
